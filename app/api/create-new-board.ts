9edab297170526fa9a7ae20086f8b45ba2d57ed4
Radoslav Figura <figura.radoslav@gmail.com>
2025-02-16 19:01:10 +0100

    Updated variables in create-new-board.ts

--- app/api/create-new-board.ts
+++ app/api/create-new-board.ts
@@ -4,7 +4,7 @@ const OK = 200 | 201 | 204
 
 const createNewBoard = async (solution: string, rows: number, cols: number) => {
     const axiosResponse = axios
-        .post(`https://${import.meta.env.API_URL}:${import.meta.env.API_PORT}/api/g`, {
+        .post(`${import.meta.env.VITE_API_URL}:${import.meta.env.VITE_API_PORT}/api/g`, {
             rows: rows,
             cols: cols,
             message: solution
