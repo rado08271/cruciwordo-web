55629a84aaad3f8b499bc956e6ef573bdcc6ec6e
Radoslav Figura <figura.radoslav@gmail.com>
2025-04-08 12:42:10 +0200

    Updated visualization of words and players

--- app/components/lists/players-list.tsx
+++ app/components/lists/players-list.tsx
@@ -10,19 +10,14 @@ const PlayersList = ({players}: Props) => {
     return (
         <>
             <ul className={'flex flex-row gap-2 min-h-8'}>
-                {players.map((player, index) => {
-                    if (index > 5) {
-                        return <TbDots className={'text-stone-300 self-center'}/>
-                    } else {
-                        return (
-                            <li key={player.id} style={{ backgroundColor: `#${player.ident3hex}`}} className={`relative rounded-full w-8 aspect-square h-8`}>
-                                <span className={`absolute aspect-square p-1 rounded-l-full rounded-tr-full bottom-0 right-0 font-light text-xs ${ player.isOnline ? 'bg-green-400' : 'bg-slate-300'}`}>
-                                    {player.foundWords.length}
-                                </span>
-                            </li>
-                        )
-                    }
-                })}
+                {players.slice(0, 5).map((player, index) => (
+                        <li key={player.id} style={{ backgroundColor: `#${player.ident3hex}`}} className={`relative rounded-full w-8 aspect-square h-8`}>
+                            <span className={`absolute aspect-square p-1 rounded-l-full rounded-tr-full bottom-0 right-0 font-light text-xs ${ player.isOnline ? 'bg-green-400' : 'bg-slate-300'}`}>
+                                {player.foundWords.length}
+                            </span>
+                        </li>
+                ))}
+                {players.length > 5 && <TbDots className={'text-stone-300 self-center'}/>}
             </ul>
         </>
     );
--- app/components/lists/words-list.tsx
+++ app/components/lists/words-list.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import type Word from "~/types/word";
+import {useSprings, animated, config} from "react-spring";
 
 type Props = {
     words: Word[]
 }
 
 const WordsList = ({words}: Props) => {
+    const [wordSprings, wordsSpringsApi] = useSprings(words.length, (idx) => ({
+        from:   { transform: "skew(0deg,-25deg)", scaleY: 0,},
+        to:     { transform: "skew(0,0)", scaleY: 1,},
+        delay:  1000 + (idx) * 50,
+        config: config.wobbly
+    }), [])
+
     return (
         <>
             <ul className={'grid grid-cols-5 md:grid-cols-3 gap-2'}>
-                {words.map(word => {
+                {words.map((word, index) => {
+                    const spring = wordSprings.at(index)
                     return (
-                        <li key={word.id} className={`relative h-8 text-xl md:text-3xl`}>
+                        <animated.li style={spring} key={word.id} className={`relative h-8 text-xl md:text-3xl`}>
                             {(word.foundBy.length === 0)
                                 ? <a className={'text-stone-500'}>{word.word}</a>
                                 : <a className={'opacity-50'} style={{color: word.foundBy.length > 0 ? `#${word.foundBy.at(0).ident3hex}` : ""}}>
                                     <strike>{word.word}</strike>
                                 </a>
                             }
-                        </li>)
+                        </animated.li>)
                 })}
             </ul>
         </>
