388aae1c559cdc473fc3ad9a2308de9223c5f581
Radoslav Figura <figura.radoslav@gmail.com>
2026-01-25 17:43:47 +0100

    chore: update package.json to add linting and formatting scripts, and update dependencies

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import typescript from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    ignores: ["node_modules", "build", ".react-router", "dist"],
+    languageOptions: {
+      parser: typescriptParser,
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: {
+      "@typescript-eslint": typescript,
+      "react-hooks": reactHooks,
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+    },
+  },
+];
